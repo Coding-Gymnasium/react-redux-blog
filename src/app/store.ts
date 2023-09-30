9063da4d28d31faa62e78a388aa6219731220fd4
Nico Rithner <nicorithner@gmail.com>
2023-09-30 12:10:36 -0600

    Implement functionality to save new posts

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import navigationReducer from "../features/navigations/navigationSlice";
+import postReducer from "../features/posts/postSlice";
 
 export const store = configureStore({
-  reducer: { navigation: navigationReducer },
+  reducer: { navigation: navigationReducer, posts: postReducer },
 });
 
 export type AppDispatch = typeof store.dispatch;
